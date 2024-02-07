ac5b56ef8254c8e94ff17f9398012e789c1e8c89
Hugo <hugosimoescontato@gmail.com>
2024-02-07 19:53:06 -0300

    feat: add note card dialog

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px] overflow-hidden">
         <NewNoteCard />
 
-        <NoteCard />
+        <NoteCard content="Hello World!" date={new Date()} />
       </div>
     </div>
   );
